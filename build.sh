d7ef5f48e1d9dc7e9fc7f7d75a9f8021505d1743
Dudecake <coen_koomen@hotmail.com>
2020-03-14 23:40:06 +0100

    Improved build script

--- build.sh
+++ build.sh
@@ -5,8 +5,16 @@ if [[ $EUD -ne 0 ]]; then
     exit 1
 fi
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )" 
 DIST_NAME=ironblue
+REPO_PATH=${REPO_PATH:-/var/lib/ostree/ironblue}
 
-rpm-ostree compose tree -r /var/cache/ostree/${DIST_NAME} --workdir /var/cache/ostree/${DIST_NAME}/tmp custom-desktop.yaml
+if [[ ! -d ${REPO_PATH}/tmp ]]; then
+    ostree init --repo=${REPO_PATH} --mode=archive
+fi
+rpm-ostree compose tree --repo=${REPO_PATH} --workdir ${REPO_PATH}/tmp ${DIR}/custom-desktop.yaml
+ostree --repo=${REPO_PATH} static-delta generate fedora/31/x86_64/ironblue
+ostree --repo=${REPO_PATH} summary -u
 #ostree pull-local /ostree/${DIST_NAME} fedora/31/${DIST_NAME}
-#ostree admin deploy fedora/31/${DIST_NAME}
+#ostree admin deploy fedora/31/x86_64/${DIST_NAME}
+#ostree admin rebase fedora/31/x86_64/${DIST_NAME}
