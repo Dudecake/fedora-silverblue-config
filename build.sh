04a7acc54944b63816ae2d05eb9b06fb880bb648
Dudecake <coen_koomen@hotmail.com>
2020-10-12 14:40:42 +0200

    Added zfs

--- build.sh
+++ build.sh
@@ -13,29 +13,32 @@ DIST_PATH=${DIST_PATH:-/srv/http/ckoomen.eu/ostree/${DIST_NAME}}
 MACHINE="$(uname -m)"
 FEDORA_VERSION=32
 
-for dir in ${REPO_PATH} ${DIST_PATH} /var/cache/ostree/${DIST_NAME}; do
+set -e
+for dir in "${REPO_PATH}" "${DIST_PATH}" "/var/cache/ostree/${DIST_NAME}"; do
   if [[ ! -d ${dir} ]]; then
     mkdir -p ${dir}
   fi
 done
-if [[ ! -d ${DIST_PATH}/tmp ]]; then
-    ostree init --repo=${DIST_PATH} --mode=archive
+
+[[ ! -z ${DRYRUN} ]] && exit 0
+if [[ ! -d "${DIST_PATH}/tmp" ]]; then
+    ostree init --repo="${DIST_PATH}" --mode=archive
     [[ "${REPO_PATH}" = "${DIST_PATH}" ]] && NEW_REPO=1
 fi
-if [[ ! -d ${REPO_PATH}/tmp ]]; then
-    ostree init --repo=${REPO_PATH}
+if [[ ! -d "${REPO_PATH}/tmp" ]]; then
+    ostree init --repo="${REPO_PATH}"
     NEW_REPO=1
 fi
-# rm -rf ${REPO_PATH}/tmp/*.tmp
-rpm-ostree compose tree --repo=${REPO_PATH} --workdir ${REPO_PATH}/tmp ${DIR}/custom-desktop.yaml
+rm -rf "${REPO_PATH}/tmp/*.tmp"
+rpm-ostree compose tree --repo="${REPO_PATH}" --workdir "${REPO_PATH}/tmp" "${DIR}/custom-desktop.yaml"
 if [[ ! -z "${NEW_REPO}" ]]; then
-    ostree --repo=${REPO_PATH} static-delta generate fedora/${FEDORA_VERSION}/${MACHINE}/${DIST_NAME}
+    ostree --repo="${REPO_PATH}" static-delta generate fedora/${FEDORA_VERSION}/${MACHINE}/${DIST_NAME}
 fi
-# ostree --repo=${REPO_PATH} gpg-sign fedora/${FEDORA_VERSION}/${MACHINE}/${DIST_NAME} ${KEY}
+# ostree --repo="${REPO_PATH}" gpg-sign fedora/${FEDORA_VERSION}/${MACHINE}/${DIST_NAME} ${KEY}
 if [[ "${REPO_PATH}" != "${DIST_PATH}" ]]; then
   ostree --repo="${DIST_PATH}" pull-local "${REPO_PATH}"
 fi
-ostree --repo=${DIST_PATH} summary -u
+ostree --repo="${DIST_PATH}" summary -u
 # Deploy with
 # ostree remote add fedora-${DIST_NAME} http://${URL}/ostree/${DIST_NAME}
 # ostree pull fedora-${DIST_NAME}:fedora/${FEDORA_VERSION}/${MACHINE}/${DIST_NAME}
