89139dc6d3751124c5555fffecac365b1a3fb297
Dudecake <coen_koomen@hotmail.com>
2022-12-09 19:54:47 +0100

    Added `ipxe-bootimgs`

--- postprocess.sh
+++ postprocess.sh
@@ -117,6 +117,7 @@ cat << EOF > /etc/NetworkManager/conf.d/wake_on_lan.conf
 ethernet.wake-on-lan=magic
 EOF
 echo /opt/rocm-*/lib > /etc/ld.so.conf.d/$(uname -m)-rocm.conf
+echo /opt/rocm-*/hsa/lib >> /etc/ld.so.conf.d/$(uname -m)-rocm.conf
 sed -i 's/#AutomaticUpdatePolicy=.*/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
 printf "%s\n" vfio vfio_iommu_type1 vfio_pci > /etc/modules-load.d/99-fedora-ironblue.conf
 # dkms install vendor-reset/$(ls -d /usr/src/vendor-reset-* | grep -Po '\d+\.\d+\.\d+') -k $(ls -1 /usr/lib/modules) --kernelsourcedir $(ls -d /usr/src/kernels/*)
